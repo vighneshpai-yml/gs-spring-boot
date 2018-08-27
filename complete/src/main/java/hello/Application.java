43be02902dbc09473fd11df718b811468726b751
vighneshpai-yml <vighnesh.pai@ymedialabs.com>
2018-08-27 16:02:32 +0530

    Update Application.java
    Updated

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -7,14 +7,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
-
+public class Application extends SpringBootServletInitializer{
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+       return application.sources(Application.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
